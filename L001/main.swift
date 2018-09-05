bebbf1a561e8b2c090e3270255f64b2f752a3835
20161104569 <a20161104569@admindeimac-88.local>
2018-09-05 11:15:53 +0800

    Initial Commit

---
+++ L001/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  L001
+//
+//  Created by 20161104569 on 18/9/5.
+//  Copyright © 2018年 20161104569. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
