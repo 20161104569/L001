25fd0332f0cf04baa910867e82f21fa775f2b963
20161104569 <2174037646@qq.com>
2018-09-12 11:35:40 +0800

    从0加到100
    利用for循环从0加到100

--- L001/main.swift
+++ L001/main.swift
@@ -6,7 +6,12 @@
 //  Copyright © 2018年 20161104569. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
-print("Hello, World!")
+var str = "Hello, playground"
 
+var sum=0;
+for  i in 1...100{
+    sum=sum+i;
+}
+print(sum)
